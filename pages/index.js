4de95f30691a6e7a7165e54edb25a4b95cfdabf3
Chandra Irugalbandara <irugalbandarachandra@gmail.com>
2021-06-07 17:26:32 +0530

    add the index (login as)
    checked the nextjs routing

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,17 @@
-import { Flex, Box, IconButton, useColorMode } from "@chakra-ui/react";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import {
+	Flex,
+	Box,
+	IconButton,
+	useColorMode,
+	Heading,
+	Button,
+} from "@chakra-ui/react";
 import { MoonIcon, SunIcon } from "@chakra-ui/icons";
 
 const index = () => {
+	const router = useRouter();
 	const { colorMode, toggleColorMode } = useColorMode();
 	return (
 		<Flex
@@ -15,7 +25,23 @@ const index = () => {
 					icon={colorMode === "light" ? <MoonIcon /> : <SunIcon />}
 				/>
 			</Box>
-			<Flex></Flex>
+			<Flex alignItems='center' justifyContent='center' direction='column'>
+				<Heading size='md' mb={5}>
+					Login As
+				</Heading>
+				<Flex alignContent='center'>
+					<Button
+						height={100}
+						width={100}
+						mr={5}
+						onClick={() => router.push("/student/dashboard")}>
+						Student
+					</Button>
+					<Button height={100} width={100}>
+						<Link href='/student/dashboard'>Recruiter</Link>
+					</Button>
+				</Flex>
+			</Flex>
 		</Flex>
 	);
 };

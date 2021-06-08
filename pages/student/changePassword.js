a6aa59899ad217e36dc6b52cd51537b25cf33e38
Chandra Irugalbandara <irugalbandarachandra@gmail.com>
2021-06-08 13:56:27 +0530

    Update changePassword.js

--- pages/student/changePassword.js
+++ pages/student/changePassword.js
@@ -1,2 +1,10 @@
 //TODO: Change password using the Previous Password and New Password.
 //TODO: Should give username as the one of the props
+
+import React from "react";
+
+const changePassword = () => {
+	return <div></div>;
+};
+
+export default changePassword;

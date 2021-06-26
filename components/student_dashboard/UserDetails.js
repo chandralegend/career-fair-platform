7f7cf09f6a8a9998942aa8c1fe32b20db8f00b3a
Chandra Irugalbandara <irugalbandarachandra@gmail.com>
2021-06-26 21:52:19 +0530

    fixed Upload CV

--- components/student_dashboard/UserDetails.js
+++ components/student_dashboard/UserDetails.js
@@ -1,148 +1,145 @@
 import {
-  Flex,
-  Avatar,
-  Heading,
-  Text,
-  useColorModeValue,
-  ButtonGroup,
-  Button,
-  useDisclosure,
-  Center,
-  Input,
-  useToast,
+	Flex,
+	Avatar,
+	Heading,
+	Text,
+	useColorModeValue,
+	ButtonGroup,
+	Button,
+	useDisclosure,
+	Center,
+	Input,
+	useToast,
 } from "@chakra-ui/react";
 import React, { useState } from "react";
 import { useRouter } from "next/router";
 
 import { useAuth } from "../../lib/auth";
-import { setCvUrl } from "../../lib/api";
+import { updateStudentCV } from "../../lib/api";
 import { firebase } from "../../lib/firebase";
 import ChangePasswordModal from "./PasswordModal";
 
 const UserDetails = () => {
-  const cardBackground = useColorModeValue("gray.100", "gray.900");
-  const { isOpen, onOpen, onClose } = useDisclosure();
-  const { user } = useAuth();
-  const toast = useToast();
-  const router = useRouter();
-  const hiddenFileInput = React.useRef(null);
+	const cardBackground = useColorModeValue("gray.100", "gray.900");
+	const { isOpen, onOpen, onClose } = useDisclosure();
+	const { user, updateUser } = useAuth();
+	const toast = useToast();
+	const router = useRouter();
+	const hiddenFileInput = React.useRef(null);
 
-  const [uploadingCV, setUploadingCV] = useState(false);
-  const [cvURL, setUserCvURL] = useState("");
+	const [uploadingCV, setUploadingCV] = useState(false);
 
-  const handleCvUpload = async (event) => {
-    const file = event.target.files[0];
+	const handleCvUpload = async (event) => {
+		const file = event.target.files[0];
 
-    if (file && file.type === "application/pdf") {
-      setUploadingCV(true);
-      try {
-        const fileName =
-          new Date().getTime().toString() +
-          (Math.ceil(Math.random() * 1000000) + 100000).toString();
+		if (file && file.type === "application/pdf") {
+			setUploadingCV(true);
+			try {
+				const fileName =
+					new Date().getTime().toString() +
+					(Math.ceil(Math.random() * 1000000) + 100000).toString();
 
-        const locationRef = firebase
-          .storage()
-          .ref("Student_CV")
-          .child(fileName);
-        await locationRef.put(file, { contentType: file.type });
-        setUserCvURL(await locationRef.getDownloadURL());
-        setUploadingCV(false);
+				const locationRef = firebase
+					.storage()
+					.ref("Student_CV")
+					.child(fileName);
+				await locationRef.put(file, { contentType: file.type });
+				locationRef.getDownloadURL().then((url) => {
+					setUploadingCV(false);
+					updateUser({ cvUrl: url });
+					updateStudentCV(user.uuid, url)
+						.then(() => {
+							toast({
+								title: "Uploaded Successfully",
+								description: "Your CV uploaded successfully",
+								status: "success",
+								duration: 5000,
+								isClosable: true,
+							});
+						})
+						.catch((err) => {
+							toast({
+								title: "Upload Failed",
+								description: "Failed to upload the CV",
+								status: "error",
+								duration: 5000,
+								isClosable: true,
+							});
+							return err;
+						});
+				});
+			} catch (error) {
+				toast({
+					title: "Upload Failed",
+					description: "Failed to upload the CV[UNKNOWN ERROR]",
+					status: "error",
+					duration: 5000,
+					isClosable: true,
+				});
+				setUploadingCV(false);
+			}
+		} else {
+			toast({
+				title: "Upload Failed",
+				description: "Please Select A Valid PDF Document",
+				status: "error",
+				duration: 5000,
+				isClosable: true,
+			});
+		}
+	};
 
-        const response = await setCvUrl(user.uuid, cvURL);
-
-        if (response && response.status === 200) {
-          toast({
-            title: "Uploaded Successfully",
-            description: "Your CV uploaded successfully",
-            status: "success",
-            duration: 5000,
-            isClosable: true,
-          });
-        } else {
-          toast({
-            title: "Upload Failed",
-            description: "Failed to upload the CV",
-            status: "error",
-            duration: 5000,
-            isClosable: true,
-          });
-        }
-      } catch (error) {
-        toast({
-          title: "Upload Failed",
-          description: "Failed to upload the CV[UNKNOWN ERROR]",
-          status: "error",
-          duration: 5000,
-          isClosable: true,
-        });
-        setUploadingCV(false);
-      }
-    } else {
-      toast({
-        title: "Upload Failed",
-        description: "Please Select A Valid PDF Document",
-        status: "error",
-        duration: 5000,
-        isClosable: true,
-      });
-    }
-  };
-
-  const handleClick = () => {
-    hiddenFileInput.current.click();
-  };
-  return (
-    <Flex width="23%" justifyContent="center">
-      <ChangePasswordModal isOpen={isOpen} onClose={onClose} />
-      <Flex
-        p={6}
-        height="-webkit-fit-content"
-        width="100%"
-        rounded={6}
-        background={cardBackground}
-        flexDirection="column"
-        alignItems="center"
-        shadow="md"
-      >
-        <Avatar size="2xl" name={user.name} src={user.photoUrl} />
-        <Heading size="md" m={4}>
-          {user.name}
-        </Heading>
-        <Flex flexDirection="column" alignItems="center">
-          <Text fontSize="small">{user.username}</Text>
-          <Text fontSize="small">{user.email}</Text>
-          <Text fontSize="small">{user.phone} (Mobile)</Text>
-          <Text fontSize="small">{user.department}</Text>
-        </Flex>
-        <Center flexDirection="column">
-          <ButtonGroup mt={4} mb={2}>
-            <Button
-              variant="solid"
-              isLoading={uploadingCV}
-              onClick={handleClick}
-            >
-              Upload CV
-            </Button>
-            <Input
-              hidden
-              type="file"
-              ref={hiddenFileInput}
-              onChange={handleCvUpload}
-            ></Input>
-            <Button
-              disabled={!user.cvUrl}
-              onClick={() => router.push(user.cvUrl)}
-            >
-              View CV
-            </Button>
-          </ButtonGroup>
-          <Button onClick={onOpen} width="100%">
-            Change Password
-          </Button>
-        </Center>
-      </Flex>
-    </Flex>
-  );
+	const handleClick = () => {
+		hiddenFileInput.current.click();
+	};
+	return (
+		<Flex width='23%' justifyContent='center'>
+			<ChangePasswordModal isOpen={isOpen} onClose={onClose} />
+			<Flex
+				p={6}
+				height='-webkit-fit-content'
+				width='100%'
+				rounded={6}
+				background={cardBackground}
+				flexDirection='column'
+				alignItems='center'
+				shadow='md'>
+				<Avatar size='2xl' name={user.name} src={user.photoUrl} />
+				<Heading size='md' m={4}>
+					{user.name}
+				</Heading>
+				<Flex flexDirection='column' alignItems='center'>
+					<Text fontSize='small'>{user.username}</Text>
+					<Text fontSize='small'>{user.email}</Text>
+					<Text fontSize='small'>{user.phone} (Mobile)</Text>
+					<Text fontSize='small'>{user.department}</Text>
+				</Flex>
+				<Center flexDirection='column'>
+					<ButtonGroup mt={4} mb={2}>
+						<Button
+							variant='solid'
+							isLoading={uploadingCV}
+							onClick={handleClick}>
+							Upload CV
+						</Button>
+						<Input
+							hidden
+							type='file'
+							ref={hiddenFileInput}
+							onChange={handleCvUpload}></Input>
+						<Button
+							disabled={!user.cvUrl}
+							onClick={() => window.open(user.cvUrl, "_blank")}>
+							View CV
+						</Button>
+					</ButtonGroup>
+					<Button onClick={onOpen} width='100%'>
+						Change Password
+					</Button>
+				</Center>
+			</Flex>
+		</Flex>
+	);
 };
 
 export default UserDetails;
--- pages/index.js
+++ pages/index.js
@@ -6,39 +6,14 @@ import {
 	useColorMode,
 	Heading,
 	Button,
-	Text,
 } from "@chakra-ui/react";
 import { MoonIcon, SunIcon } from "@chakra-ui/icons";
+
 import { StudentIcon, CompanyIcon } from "../components/icons";
-import { db } from "../lib/firebase";
-import { useEffect, useState } from "react";
 
 const index = () => {
 	const router = useRouter();
 	const { colorMode, toggleColorMode } = useColorMode();
-
-	const [data, setData] = useState([]);
-	function snapShotTest() {
-		try {
-			db.collection("test").onSnapshot((snapshot) => {
-				const new_data = [];
-				snapshot.forEach((doc) => {
-					new_data.push(doc.data());
-				});
-				console.log(
-					"🚀 ~ file: index.js ~ line 25 ~ db.collection ~ new_data",
-					new_data
-				);
-				setData(new_data);
-			});
-		} catch (error) {
-			console.log(error);
-		}
-	}
-
-	useEffect(() => {
-		snapShotTest();
-	}, []);
 
 	return (
 		<Flex
@@ -53,7 +28,6 @@ const index = () => {
 				/>
 			</Box>
 			<Flex alignItems='center' justifyContent='center' direction='column'>
-				<Text>{JSON.stringify(data)}</Text>
 				<Heading size='md' mb={5}>
 					Login As
 				</Heading>

8877f219e7bb41013878232398b1fe7e2f89d2c9
JanithSamee <janith.samee0@gmail.com>
2021-07-23 18:08:40 +0530

    added check-in functionality

--- components/company_dashboard/CurrentInterview.js
+++ components/company_dashboard/CurrentInterview.js
@@ -3,12 +3,12 @@ import { useEffect, useState } from "react";
 
 import { useInterview } from "../../lib/interviews";
 import CandidateDetails from "./CandidateDetails";
-import { getStudent } from "../../lib/api";
+import { getStudent, createMeeting } from "../../lib/api";
 
-const CurrentInterview = () => {
+const CurrentInterview = ({ session }) => {
 	const { inQueueInterviews } = useInterview();
 	const [student, setStudent] = useState();
-
+	const [isloading, setisloading] = useState(false);
 	useEffect(() => {
 		if (inQueueInterviews.length) {
 			console.count("current Interview get Student");
@@ -21,30 +21,42 @@ const CurrentInterview = () => {
 	}, [inQueueInterviews]);
 
 	return (
-		<Flex p={3} flexDirection='column' width='100%'>
-			<Heading size='md'>Current Interview</Heading>
+		<Flex p={3} flexDirection="column" width="100%">
+			<Heading size="md">Current Interview</Heading>
 			<CandidateDetails data={student} />
 			<ButtonGroup mt={3}>
 				<Button
 					flex={1}
-					colorScheme='green'
-					shadow='md'
-					rounded='full'
-					disabled={!inQueueInterviews.length}
-					onClick={() => {
-						window.open(inQueueInterviews[0] && inQueueInterviews[0].meet_link, "_blank");
-					}}>
+					colorScheme="green"
+					shadow="md"
+					rounded="full"
+					isLoading={isloading}
+					disabled={!inQueueInterviews.length && !session}
+					onClick={async () => {
+						setisloading(true);
+						createMeeting({
+							interviewId: inQueueInterviews[0].id,
+							sessionTime: session.start_time._seconds * 1000,
+						})
+							.then((response) => {
+								setisloading(false);
+								window.open(response.data, "_blank");
+							})
+							.catch((e) => console.log(e));
+					}}
+				>
 					Join Meeting
 				</Button>
 				<Button
 					flex={1}
-					colorScheme='orange'
-					shadow='md'
-					rounded='full'
+					colorScheme="orange"
+					shadow="md"
+					rounded="full"
 					disabled={!inQueueInterviews.length}
 					onClick={() => {
 						window.open(inQueueInterviews[0] && "https://www.google.com/forms/about/", "_blank");
-					}}>
+					}}
+				>
 					Feedback
 				</Button>
 			</ButtonGroup>
--- components/student_dashboard/SessionCard.js
+++ components/student_dashboard/SessionCard.js
@@ -1,8 +1,9 @@
 import { Flex, Avatar, Text, Button, Skeleton } from "@chakra-ui/react";
 import React, { useState, useEffect } from "react";
 import { db } from "../../lib/firebase";
-import { getPanel, getCompany } from "../../lib/api";
+import { getPanel, getCompany, createInterview } from "../../lib/api";
 import moment from "moment";
+import { useAuth } from "../../lib/auth";
 
 async function formatData(session_data) {
 	const panel = await getPanel(session_data.panel_id);
@@ -13,17 +14,17 @@ async function formatData(session_data) {
 		company_name: company.data.name,
 		company_logo: company.data.photoUrl,
 		interview_schedule: session_data.start_time,
+		company_id: panel.data.company_id,
 	};
 }
 
-//TODO: Add Checkin Fuctionality @Janith
-
 const SessionCard = ({ session }) => {
 	const [
-		{ company_logo, company_name, assigned_students, isCheckinEnabled, interview_schedule, panel_no },
+		{ company_logo, company_name, assigned_students, isCheckinEnabled, interview_schedule, panel_no, company_id },
 		setSession,
 	] = useState({});
 	const [loading, setLoading] = useState(true);
+	const { user } = useAuth();
 
 	const fetchSession = () => {
 		try {
@@ -43,36 +44,59 @@ const SessionCard = ({ session }) => {
 		}
 	};
 
+	const handleCheckIn = async (session, userID) => {
+		if (session != undefined && userID != undefined) {
+			const data = {
+				studentId: userID,
+				sessionId: session.id,
+				isWalkin: false,
+				companyId: company_id,
+			};
+			await createInterview(data);
+		} else {
+			//TODO: handle error
+			console.log("cannot create");
+		}
+	};
+
 	useEffect(() => {
 		const unsubscribe = fetchSession();
 		return () => unsubscribe();
 	}, []);
 
 	return (
-		<Flex boxShadow='base' p={3} flexDirection='column' mt={5} width='100%' rounded='md'>
-			<Skeleton isLoaded={!loading} rounded='xl' fadeDuration={3}>
-				<Flex alignItems='center'>
-					<Flex width='30%'>
-						<Avatar size='md' src={company_logo} mr={3} backgroundColor='white' boxShadow='lg' />
-						<Flex flexDirection='column'>
-							<Text noOfLines={1} fontSize='larger'>
+		<Flex boxShadow="base" p={3} flexDirection="column" mt={5} width="100%" rounded="md">
+			<Skeleton isLoaded={!loading} rounded="xl" fadeDuration={3}>
+				<Flex alignItems="center">
+					<Flex width="30%">
+						<Avatar size="md" src={company_logo} mr={3} backgroundColor="white" boxShadow="lg" />
+						<Flex flexDirection="column">
+							<Text noOfLines={1} fontSize="larger">
 								{company_name}
 							</Text>
-							<Text fontSize='smaller'>Panel {panel_no}</Text>
+							<Text fontSize="smaller">Panel {panel_no}</Text>
 						</Flex>
 					</Flex>
-					<Flex width='55%'>
-						<Text fontSize='large'>{assigned_students && assigned_students.length} people are assigned</Text>
+					<Flex width="55%">
+						<Text fontSize="large">
+							{assigned_students && assigned_students.length} people are assigned
+						</Text>
 					</Flex>
-					<Flex width='15%'>
-						<Button colorScheme='blue' disabled={!isCheckinEnabled} rounded='full'>
+					<Flex width="15%">
+						<Button
+							colorScheme="blue"
+							disabled={!isCheckinEnabled || user.checkedin}
+							rounded="full"
+							onClick={() => handleCheckIn(session, user.uuid)}
+						>
 							Check-In
 						</Button>
 					</Flex>
 				</Flex>
-				<Flex justifyContent='flex-end'>
-					<Text fontSize='xs'>
-						{interview_schedule && moment.unix(interview_schedule.seconds).format("dddd, MMMM Do YYYY, h:mm:ss a")}
+				<Flex justifyContent="flex-end">
+					<Text fontSize="xs">
+						{interview_schedule &&
+							moment.unix(interview_schedule.seconds).format("dddd, MMMM Do YYYY, h:mm:ss a")}
 					</Text>
 				</Flex>
 			</Skeleton>
